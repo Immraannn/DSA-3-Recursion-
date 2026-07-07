36d59f67e9d6f81441dca5a8878df2662fe023ef
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-08 00:29:14 +0530

    Add comments for Fibonacci function visualization
    Added comments to illustrate the Fibonacci function's recursive calls.

--- fibbonaci04.cpp
+++ fibbonaci04.cpp
@@ -20,3 +20,16 @@ int main() {
 
     return 0;
 }
+//output
+//5
+//5
+
+ //                         fib(5)
+ //                       /        \
+ //                  fib(4)        fib(3)
+ //                 /     \        /     \
+ //            fib(3)   fib(2)  fib(2)  fib(1)
+ //           /    \     /   \    /   \
+ //      fib(2) fib(1) fib(1) fib(0) fib(1) fib(0)
+ //      /   \
+ // fib(1) fib(0)
