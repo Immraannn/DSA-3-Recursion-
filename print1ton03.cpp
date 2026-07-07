9195f794ea03b55a9bcb0d4de65cb73b95645622
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-08 00:10:36 +0530

    Implement recursive function to print numbers 1 to n

---
+++ print1ton03.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void printCount(int n) {
+    // Base case
+    if (n == 0)
+        return;
+
+    // Recursive call
+    printCount(n - 1);
+
+    // Print after recursion
+    cout << n << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printCount(n);
+
+    return 0;
+}
