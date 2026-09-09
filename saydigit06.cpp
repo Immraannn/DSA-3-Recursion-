3dd646a031ac999685485257ce615f91a2453aa2
Md Imran Shakeel <imshkl2005@gmail.com>
2026-09-09 22:44:57 +0530

    Update saydigit06.cpp

--- saydigit06.cpp
+++ saydigit06.cpp
@@ -2,19 +2,14 @@
 using namespace std;
 
 void sayDigit(int n, string arr[]) {
-
     //base case
     if(n == 0)
         return ;
-
     //processing 
     int digit = n % 10;
     n = n / 10;
-    
-
     //recursive call
     sayDigit(n, arr);
-
     cout << arr[digit] << " ";
 
 }
